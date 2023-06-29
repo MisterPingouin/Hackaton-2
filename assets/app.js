b2d1eb92f8c1a6f639c9803a0362e5944ff534f7
MisterPingouin <anthony.axeldisplay@gmail.com>
2023-06-29 02:28:55 +0200

    some fix

--- assets/app.js
+++ assets/app.js
@@ -40,10 +40,22 @@ document.getElementById('phone_marque').addEventListener('click', function() {
         document.getElementById('phone_modele').innerHTML = '';
     }
 });
+// Ajoutez cet écouteur d'événement dans votre fichier JavaScript existant
 document.addEventListener("DOMContentLoaded", function() {
-    // ...
-    var tooltipTriggerList = document.querySelectorAll('#choice-etat [data-bs-original-title]');
-    var tooltipList = Array.from(tooltipTriggerList).map(function(tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl);
-    });
-});
+    // Fonction pour ajouter l'animation d'entrée depuis la gauche aux champs du formulaire
+    function animateFormField(fieldId) {
+      var field = document.getElementById(fieldId);
+      field.addEventListener('animationend', function() {
+        field.classList.remove('slide-in-left');
+        field.style.animation = '';
+      });
+    }
+  
+    // Appel de la fonction pour chaque champ du formulaire
+    animateFormField('choice-marque');
+    animateFormField('choice-modele');
+    animateFormField('choice-ram');
+    animateFormField('choice-stockage');
+    animateFormField('choice-etat');
+  });
+  
